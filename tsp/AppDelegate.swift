091f9c9033434ed1fb3d45867ca8f6a14db9a002
Mary <marygerina1204@gmail.com>
2019-02-25 14:45:38 +0200

    Initial Commit

---
+++ tsp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  tsp
+//
+//  Created by Mary Gerina on 2/25/19.
+//  Copyright © 2019 Mary Gerina. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ tsp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tsp
+//
+//  Created by Mary Gerina on 2/25/19.
+//  Copyright © 2019 Mary Gerina. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
